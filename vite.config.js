2b128618382de67f6e8d6c8e9a3c4f73f16b4bc7
mnorlin <1668384+mnorlin@users.noreply.github.com>
2023-01-22 21:46:49 +0100

    ci(github): Streamline publish to Github pages

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,8 @@
-import { defineConfig } from 'vite'
-import { svelte } from '@sveltejs/vite-plugin-svelte'
+import { defineConfig } from "vite";
+import { svelte } from "@sveltejs/vite-plugin-svelte";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [svelte()],
-  build: {
-    outDir: './docs'
-  }
-})
+  base: "/scrum-wheel/",
+});
